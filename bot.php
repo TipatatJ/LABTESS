8c8c3a0f91b4415836eca0d38228a4f8d85be9bf
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-03-10 15:36:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
             // Build message to reply back
             $messages = [
                 'type' => 'text',
-                'text' => $text,
+                'text' => $content,
             ];
             // Make a POST Request to Messaging API to reply to sender
             $url = 'https://api.line.me/v2/bot/message/reply';
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) {
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
             $result = curl_exec($ch);
             curl_close($ch);
-            echo $content. "";
+            echo $text. "";
         }
     }
 }
