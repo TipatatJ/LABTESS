962bcf1aa9919f4cd0054470312e924578503bce
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-03-10 16:31:51 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -39,7 +39,7 @@ function processMessage($update) {
     }else{
         sendMessage(array(
             "source" => $update["responseId"],
-            "fulfillmentText"=>"Error!!!",
+            "fulfillmentText"=>"Error!!! ".json_encode($update),
             "payload" => array(
                 "items"=>[
                     array(
