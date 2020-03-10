77733f0fed9c0fe514c1ac91e99a3b3c522793ea
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-03-10 16:09:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,7 +31,7 @@ if (!is_null($events['events'])) {
             // Build message to reply back
             $messages = [
                 'type' => 'text',
-                'text' => $content,
+                'text' => json_encode($POST),
             ];
         }
 
