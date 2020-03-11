aa85f20d70502a9cf447afd58dc1bd27934e951b
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-03-11 16:40:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -106,7 +106,7 @@ if (!is_null($events['events'])) {
 echo $text;
 
 
-function getNearByCAirPM($Lat,$Long){
+function getNearByCAirPM($lat1,$lon1){
 
     # Our new data
     $data = array(
@@ -143,11 +143,68 @@ function getNearByCAirPM($Lat,$Long){
     //echo '<br>'.$curlResponse;
     //echo '<hr>';
 
-    //$jsonC_air = json_decode('['.$curlResponse.']', true);
+    $jsonC_air = json_decode('['.$curlResponse.']', true);
 
-    
+    $closestStation = array('distance'=>'9999');
+
+    foreach($jsonC_air as $key=>$arrStation){
+        $lat2 = $arrStation['latitude'];
+        $lon2 = $arrStation['longitude'];
+        $arrStation['distance'] = distance($lat1, $lon1, $lat2, $lon2, 'K')
+        if($arrStation['distance'] < $closestStation['distance']){
+            $closestStation = $arrStation;
+        }
+    }
 
     //return $jsonC_air;
-    return substr($urlResponse, 0, 400);
+    return json_encode($closestStation);
+}
+
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::                                                                         :*/
+/*::  This routine calculates the distance between two points (given the     :*/
+/*::  latitude/longitude of those points). It is being used to calculate     :*/
+/*::  the distance between two locations using GeoDataSource(TM) Products    :*/
+/*::                                                                         :*/
+/*::  Definitions:                                                           :*/
+/*::    South latitudes are negative, east longitudes are positive           :*/
+/*::                                                                         :*/
+/*::  Passed to function:                                                    :*/
+/*::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :*/
+/*::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :*/
+/*::    unit = the unit you desire for results                               :*/
+/*::           where: 'M' is statute miles (default)                         :*/
+/*::                  'K' is kilometers                                      :*/
+/*::                  'N' is nautical miles                                  :*/
+/*::  Worldwide cities and other features databases with latitude longitude  :*/
+/*::  are available at https://www.geodatasource.com                          :*/
+/*::                                                                         :*/
+/*::  For enquiries, please contact sales@geodatasource.com                  :*/
+/*::                                                                         :*/
+/*::  Official Web site: https://www.geodatasource.com                        :*/
+/*::                                                                         :*/
+/*::         GeoDataSource.com (C) All Rights Reserved 2018                  :*/
+/*::                                                                         :*/
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+  if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+    return 0;
+  }
+  else {
+    $theta = $lon1 - $lon2;
+    $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+    $dist = acos($dist);
+    $dist = rad2deg($dist);
+    $miles = $dist * 60 * 1.1515;
+    $unit = strtoupper($unit);
+
+    if ($unit == "K") {
+      return ($miles * 1.609344);
+    } else if ($unit == "N") {
+      return ($miles * 0.8684);
+    } else {
+      return $miles;
+    }
+  }
 }
 ?>
