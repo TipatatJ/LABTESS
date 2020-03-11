a6b360896cc22857160fc1fefbaf6cf25063b777
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-03-11 13:40:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -68,7 +68,7 @@ if (!is_null($events['events'])) {
 
             $messages = [
                 'type' => 'text',
-                'text' => ,
+                'text' => '???',
             ];
         }
         else{
