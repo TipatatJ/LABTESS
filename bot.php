850c87da5820655869fe6072db7e9a38716b2d27
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-03-11 15:40:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -99,7 +99,7 @@ if (!is_null($events['events'])) {
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
             $result = curl_exec($ch);
             curl_close($ch);
-            echo $text. "";
+            echo "%".$text. "%";
     }
 }
 echo $text;
