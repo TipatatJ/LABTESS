e1bd105b6140ee147b493aaeb2054a3f36114c7b
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-03-12 17:22:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -94,13 +94,13 @@ if (!is_null($events['events'])) {
                             "type": "postback",
                             "label": "คำแนำนำสำหรับคนปกติ",
                             "text": "PMgeneralAdvise",
-                            "data": "{"of":"PMgeneralAdvise","PM2.5":'.$text['pm2.5'].'}"
+                            "data": "{\"of\":\"PMgeneralAdvise\",\"PM2.5\":'.$text['pm2.5'].'}"
                         },
                         {
                             "type": "postback",
                             "label": "คำแนะนำคนมีความเสี่ยง",
                             "text": "PMriskAdvise",
-                            "data": "{"of":"PMriskAdvise","PM2.5":'.$text['pm2.5'].'}"
+                            "data": "{\"of\":\"PMriskAdvise\",\"PM2.5\":'.$text['pm2.5'].'}"
                         }
                         ],
                         "title": "สภาพอากาศพื้นที่ใกล้เคียง",
