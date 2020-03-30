da9a2cb70f53c24aca667a9432834a2b08055690
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-03-30 21:14:51 +0700

    Update botHomeoMap.php

--- botHomeoMap.php
+++ botHomeoMap.php
@@ -222,7 +222,7 @@ function getUserLastMessage($userId){
 
     $rtnWTH = file_get_contents($url.'?userId='.$userId);
     $lastSMS = json_decode($rtnWTH);
-    return $lastSMS[0]['sms_txt'];
+    return $lastSMS['sms_txt'];
 }
 
 function getNearByCAirPM($lat1,$lon1){
