7d0e2129bbfc1798c82c35eec47bbeb9a51dec94
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-03-30 13:06:15 +0700

    Update botHomeoMap.php

--- botHomeoMap.php
+++ botHomeoMap.php
@@ -147,7 +147,7 @@ if (!is_null($events['events'])) {
         // Make a POST Request to Messaging API to reply to sender
              $url = 'https://api.line.me/v2/bot/message/reply';
             $data = [
-                'replyToken' => $replyToken['source']['userId'],
+                'replyToken' => $replyToken['source'],
                 'messages' => [$messages]
             ];
             $post = json_encode($data);
