8e42e3d79dfef8751aaed214fe078800c1b3c6e3
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-03-31 09:01:36 +0700

    try buttons post back for Anonymous user

--- lastMessageHandler.php
+++ lastMessageHandler.php
@@ -12,6 +12,32 @@
                 ',
             ];
 
+            $jsonMsg = '{
+                "type": "template",
+                "altText": "this is a buttons template",
+                    "template": {
+                        "type": "buttons",
+                        "actions": [
+                        {
+                            "type": "postback",
+                            "label": "คำแนำนำสำหรับคนปกติ",
+                            "text": "PMgeneralAdvise",
+                            "data": "PMgeneralAdvise,'.$text['pm2.5'].'"
+                        },
+                        {
+                            "type": "postback",
+                            "label": "คำแนะนำคนมีความเสี่ยง",
+                            "text": "PMriskAdvise",
+                            "data": "PMriskAdvise,'.$text['pm2.5'].'"
+                        }
+                        ],
+                        "title": "สภาพอากาศพื้นที่ใกล้เคียง",
+                        "text": "มีปริมาณ PM2.5 ที่ระดับ '.$text['pm2.5'].' mcg/m3"
+                    }
+                }';
+
+            $messages = json_decode($jsonMsg, true);
+
             $fields = array(
             "userId"=>$userId,
             "txt"=>'WTH name as Anonymous', 
