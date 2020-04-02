b6539acfb4bc2f8467a24d8f0a070dc857da38dc
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-04-02 13:48:30 +0700

    try Reasking if user reply with Sticker or Image

--- botHomeoMap.php
+++ botHomeoMap.php
@@ -213,10 +213,32 @@ if (!is_null($events['events'])) {
                 // Get replyToken
                 $replyToken = $event['replyToken'];
                 // Build message to reply back
-                $messages = [
+                /* $messages = [
                     'type' => 'text',
                     'text' => 'NON MESSAGE TYPE\n'.$content,
+                ]; */
+
+                //$decContent = json_decode($content, true);
+                $userMsgType = $event['message']['type'];
+
+                $messages = [
+                    'type' => 'text',
+                    'text' => '
+                    เรายังไม่สามารถเข้าใจ '.$userMsgType.' ได้
+                    ต้องรบกวนตอบคำถามด้วยการพิมพ์ตอบอีกครั้ง
+                    ',
                 ];
+
+                
+                /* $fields = array(
+                "userId"=>$userId,
+                "txt"=>json_encode(array('WTH'=>'use case experience'),JSON_UNESCAPED_UNICODE), 
+                "me"=>$me);
+                post2WTH($fields); */
+
+                justMsg($messages, $replyToken, $access_token);
+                exit;
+                break;
             //}
         }
 
