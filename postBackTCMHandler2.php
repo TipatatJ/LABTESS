6c267afa04b0989952aae3023008e15da83c5194
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-04-06 20:55:23 +0700

    minor change

--- postBackTCMHandler2.php
+++ postBackTCMHandler2.php
@@ -84,7 +84,7 @@
 
             $fields = array(
             "userId"=>$userId,
-            "txt"=>json_encode(array('WTH'=>'serve acup'),JSON_UNESCAPED_UNICODE), 
+            "txt"=>json_encode(array('WTH'=>'serve herb'),JSON_UNESCAPED_UNICODE), 
             "me"=>$me);
             post2WTH($fields);
 
@@ -103,7 +103,7 @@
 
             $fields = array(
             "userId"=>$userId,
-            "txt"=>json_encode(array('WTH'=>'serve herb'),JSON_UNESCAPED_UNICODE), 
+            "txt"=>json_encode(array('WTH'=>'serve tuina'),JSON_UNESCAPED_UNICODE), 
             "me"=>$me);
             post2WTH($fields);
 
