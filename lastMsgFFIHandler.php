12bb143be0f9644da93a11686897e2e305d0d00f
TipatatJ <59468637+TipatatJ@users.noreply.github.com>
2020-04-22 14:14:35 +0700

    Update lastMsgFFIHandler.php

--- lastMsgFFIHandler.php
+++ lastMsgFFIHandler.php
@@ -432,43 +432,11 @@
                     "actions": [
                         {
                         "type": "message",
-                        "text": "Blogger",
-                        "label": "Blogger"
-                        },
-                        {
-                        "type": "message",
-                        "text": "Youtuber",
-                        "label": "Youtuber"
-                        },
-                        {
-                        "type": "message",
-                        "text": "Live video",
-                        "label": "Live video"
+                        "text": "I Street",
+                        "label": "I Street"
                         }
                     ],
-                    "thumbnailImageUrl": "https://www.venitaclinic.com/LABTESS/infoMap/images/Review.jpg"
-                    },
-                    {
-                    "title": "Marketing",
-                    "text": "consultant",
-                    "actions": [
-                        {
-                        "type": "message",
-                        "text": "Commercial",
-                        "label": "Commercial"
-                        },
-                        {
-                        "type": "message",
-                        "text": "...",
-                        "label": "..."
-                        },
-                        {
-                        "type": "message",
-                        "text": "...",
-                        "label": "..."
-                        }
-                    ],
-                    "thumbnailImageUrl": "https://www.venitaclinic.com/LABTESS/infoMap/images/Review.jpg"
+                    "thumbnailImageUrl": "https://www.venitaclinic.com/LABTESS/infoMap/images/I_cook_serve.jpg"
                     }
                 ]
                 },
